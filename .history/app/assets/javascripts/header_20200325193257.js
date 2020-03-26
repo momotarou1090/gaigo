d77396ea1995afcd3646281cbaf5e17b5dab557c
momotarou1090 <59251343+momotarou1090@users.noreply.github.com>
2020-03-26 15:12:50 +0900

    headerを作成

---
+++ .history/app/assets/javascripts/header_20200325193257.js
@@ -0,0 +1,69 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $(function(){
+    $(".toggle-icon").on("click", function() {
+      $(".toggle-opened").slideToggle();
+    });
+    $("toggle-icon").on("click", function() {
+      $("toggle-icon").hide();
+      return false;
+    });
+    $(".x-icon").on("click", function() {
+      $("#acMenu dd").hide();
+      return false;
+    });
+    $(".x-icon").on("click", function() {
+      $("#acMenu dt").show();
+    });
+  });
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  $('.hover-home').hover(
+    function() {
+      $('.hover-home').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-home').css('color', '#2ea3f2');
+    }
+  );
+  $('.hover-plofile').hover(
+    function() {
+      $('.hover-plofile').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-plofile').css('color', '#2ea3f2');
+    }
+  );
+  $('.hover-event').hover(
+    function() {
+      $('.hover-event').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-event').css('color', '#2ea3f2');
+    }
+  );
+  $('.hover-history').hover(
+    function() {
+      $('.hover-history').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-history').css('color', '#2ea3f2');
+    }
+  );
+  $('.hover-movie').hover(
+    function() {
+      $('.hover-movie').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-movie').css('color', '#2ea3f2');
+    }
+  );
+  $('.hover-contact').hover(
+    function() {
+      $('.hover-contact').css('color', '#378cc5');
+    },
+    function() {
+      $('.hover-contact').css('color', '#2ea3f2');
+    }
+  );
+});
--- .history/app/assets/javascripts/header_20200325202916.js
+++ .history/app/assets/javascripts/header_20200325202916.js
@@ -1,7 +1,26 @@
 window.addEventListener('DOMContentLoaded', function(){
+  $(function(){
+    $(".toggle-icon").on("click", function() {
+      $(".toggle-opened").slideToggle();
+    });
+    $("toggle-icon").on("click", function() {
+      $("toggle-icon").hide();
+      return false;
+    });
+    $(".x-icon").on("click", function() {
+      $(".toggle-opened").hide();
+      return false;
+    });
+    $(".x-icon").on("click", function() {
+      $(".toggle-icon").show();
+    });
+  });
+});
+
+window.addEventListener('DOMContentLoaded', function(){
   $('.hover-home').hover(
     function() {
-      $('.hover-home').css('color', '#378cc5');
+      $('.hover-home').css('color', 'skyblue');
     },
     function() {
       $('.hover-home').css('color', '#2ea3f2');
@@ -9,7 +28,7 @@ window.addEventListener('DOMContentLoaded', function(){
   );
   $('.hover-plofile').hover(
     function() {
-      $('.hover-plofile').css('color', '#378cc5');
+      $('.hover-plofile').css('color', 'skyblue');
     },
     function() {
       $('.hover-plofile').css('color', '#2ea3f2');
@@ -17,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function(){
   );
   $('.hover-event').hover(
     function() {
-      $('.hover-event').css('color', '#378cc5');
+      $('.hover-event').css('color', 'skyblue');
     },
     function() {
       $('.hover-event').css('color', '#2ea3f2');
@@ -25,7 +44,7 @@ window.addEventListener('DOMContentLoaded', function(){
   );
   $('.hover-history').hover(
     function() {
-      $('.hover-history').css('color', '#378cc5');
+      $('.hover-history').css('color', 'skyblue');
     },
     function() {
       $('.hover-history').css('color', '#2ea3f2');
@@ -33,7 +52,7 @@ window.addEventListener('DOMContentLoaded', function(){
   );
   $('.hover-movie').hover(
     function() {
-      $('.hover-movie').css('color', '#378cc5');
+      $('.hover-movie').css('color', 'skyblue');
     },
     function() {
       $('.hover-movie').css('color', '#2ea3f2');
@@ -41,10 +60,11 @@ window.addEventListener('DOMContentLoaded', function(){
   );
   $('.hover-contact').hover(
     function() {
-      $('.hover-contact').css('color', '#378cc5');
+      $('.hover-contact').css('color', 'skyblue');
     },
     function() {
       $('.hover-contact').css('color', '#2ea3f2');
     }
   );
+  $
 });
